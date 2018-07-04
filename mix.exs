e2145cddd6b5fd8a27b0557982a9e165fc664907
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-07-04 13:55:53 -0400

    Version 0.2.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Artificery.MixProject do
   def project do
     [
       app: :artificery,
-      version: "0.2.4",
+      version: "0.2.5",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
